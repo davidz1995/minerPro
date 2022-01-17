57e69bd3254ad79c637c8d2943115db5ead5ebbc
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-17 18:48:39 -0300

    Creado backend y DB

---
+++ backend/src/models/User.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+  sequelize.define('user', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+};
